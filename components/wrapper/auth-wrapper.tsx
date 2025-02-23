d92b3238c7aceee598977a67cdcfa2ec3ea20d19
Mohd Yasin <work.mohdyasin@gmail.com>
2025-02-23 22:28:51 +0530

    Update auth-wrapper.tsx

--- components/wrapper/auth-wrapper.tsx
+++ components/wrapper/auth-wrapper.tsx
@@ -14,6 +14,10 @@ const AuthWrapper = ({ children }: AuthWrapperProps) => {
   return (
     <ClerkProvider
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      signInUrl={process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL}
+      signUpUrl={process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL}
+      signInFallbackRedirectUrl={process.env.NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL}
+      signUpFallbackRedirectUrl={process.env.NEXT_PUBLIC_CLERK_SIGN_UP_FALLBACK_REDIRECT_URL}
       appearance={{
         baseTheme: dark,
         layout: {
