a8d1624bf96367f3ff407cab0e5f8514671b5eb8
Mohd Yasin <work.mohdyasin@gmail.com>
2025-02-23 21:42:31 +0530

    removed env and fixed some bugs`

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,5 @@
 import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { env } from "@/app/env";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -35,10 +34,6 @@ export function slugify(text: string) {
     .replace(/--+/g, "-") // Replace multiple - with single -
     .replace(/^-+/, "") // Trim - from start of text
     .replace(/-+$/, "") // Trim - from end of text
-}
-
-export function absoluteUrl(path: string) {
-  return `${env.NEXT_PUBLIC_APP_URL}${path}`
 }
 
 /**
