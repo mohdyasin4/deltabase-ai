6acfa51b0907c939bf55397c9ac62de2ed499acc
Mohd Yasin <work.mohdyasin@gmail.com>
2025-02-25 19:48:25 +0530

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_SUPABASE_SERVICE_ROLE_KEYas string
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_DATABASE_URL as string
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_SUPABASE_SERVICE_ROLE_KEY as string
 console.log("supbase Url:",supabaseUrl)
 console.log("supbase Key:",supabaseKey)
